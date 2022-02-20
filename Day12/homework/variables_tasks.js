f9549a406d42645f75d296d062a7199045760755
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-02-20 19:20:21 +0200

    solve the class weekend boring task

---
+++ Day12/homework/variables_tasks.js
@@ -0,0 +1,20 @@
+// Read: https://javascript.info/variables + Tasks
+
+// Working with variables
+// Declare two variables: admin and name.
+let admin;
+let name;
+// Assign the value "John" to name.
+name = "John";
+// Copy the value from name to admin.
+admin = name;
+// Show the value of admin using alert (must output “John”).
+alert(admin);
+// Giving the right name
+// Create a variable with the name of our planet. How would you name such a variable?
+let ourPlanet = "Earth";
+// Create a variable to store the name of a current visitor to a website. How would you name that variable?
+let currentUser = "Aws";
+// Uppercase const?
+const BIRTHDAY = "18.04.1982"; // never changes
+const age = someCode(BIRTHDAY); // will change each year
