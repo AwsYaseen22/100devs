64f0294f6a4312ad228daf29c2b182c2a3c03116
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-03-20 14:48:46 +0300

    solve some katas

--- codeWars/code.js
+++ codeWars/code.js
@@ -39,4 +39,212 @@ function fakeBin(x) {
     })
     .join("");
 }
-console.log(fakeBin("45385593107843568"));
+// console.log(fakeBin("45385593107843568"));
+
+// ##################################################
+
+// Is he gonna survive?
+function hero(bullets, dragons) {
+  return dragons * 2 <= bullets;
+}
+
+// console.log(hero(10, 6));
+
+// ##################################################
+
+// Calculate BMI
+
+function bmi(weight, height) {
+  let bmiIndex = weight / Math.pow(height, 2);
+  if (bmiIndex <= 18.5) {
+    return "Underweight";
+  } else if (bmiIndex <= 25) {
+    return "Normal";
+  } else if (bmiIndex <= 30) {
+    return "Overweight";
+  } else if (bmiIndex > 30) {
+    return "Obese";
+  }
+}
+
+// console.log(bmi(80, 1.8));
+
+// ##################################################
+// Calculate average
+
+function find_average(array) {
+  let sum = 0;
+  if (array.length === 0) {
+    return sum;
+  }
+  array.forEach((n) => (sum += n));
+  return sum / array.length;
+}
+// console.log(find_average([1, 2, 3]));
+
+// ##################################################
+// Reversed sequence
+
+const reverseSeq = (n) => {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.unshift(i);
+  }
+  return arr;
+};
+// console.log(reverseSeq(0));
+
+// ##################################################
+
+// Beginner Series #1 School Paperwork
+function paperwork(n, m) {
+  if (n <= 0 || m <= 0) {
+    return 0;
+  }
+  return n * m;
+}
+// console.log(paperwork(5, 5));
+
+// ##################################################
+
+// DNA to RNA Conversion
+function DNAtoRNA(dna) {
+  let rna = dna.split("").map((s) => {
+    return s === "T" ? (s = "U") : s;
+  });
+  return rna.join("");
+}
+// console.log(DNAtoRNA("GCAT"));
+
+// ##################################################
+
+// You only need one - Beginner
+
+function check(a, x) {
+  return a.includes(x);
+}
+// console.log(check([66, 101], 661));
+
+// ##################################################
+
+// Sum Arrays
+
+function sum(numbers) {
+  "use strict";
+  return numbers.reduce((a, s) => a + s, 0);
+}
+// console.log(sum([]));
+
+// ##################################################
+// Are You Playing Banjo?
+
+function areYouPlayingBanjo(name) {
+  return name[0].toLowerCase() === "r"
+    ? name + " plays banjo"
+    : name + " does not play banjo";
+}
+// console.log(areYouPlayingBanjo("Adam"));
+
+// ##################################################
+
+// Opposites Attract
+
+function lovefunc(flower1, flower2) {
+  let f1 = flower1 % 2 === 0;
+  let f2 = flower2 % 2 === 0;
+  return f1 !== f2;
+}
+// console.log(lovefunc(2, 4));
+
+// ##################################################
+
+// Find Maximum and Minimum Values of a List
+
+var min = function (list) {
+  let minimum = list[0];
+  list.forEach((n) => {
+    if (n < minimum) {
+      minimum = n;
+    }
+  });
+  return minimum;
+};
+
+var max = function (list) {
+  let maximum = list[0];
+  list.forEach((n) => {
+    if (n > maximum) {
+      maximum = n;
+    }
+  });
+  return maximum;
+};
+
+// console.log(min([-52, 56, 30, 29, -54, 0, -110]));
+// console.log(max([5]));
+
+// ##################################################
+
+// Array plus array
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((a, c) => a + c, 0); //something went wrong
+}
+
+// ##################################################
+
+// Will you make it?
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  return mpg * fuelLeft >= distanceToPump;
+};
+
+// console.log(zeroFuel(51, 25, 2));
+
+// ##################################################
+
+// Count the Monkeys!
+
+function monkeyCount(n) {
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+// console.log(monkeyCount(5));
+
+// ##################################################
+
+// Simple multiplication
+
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8 : number * 9;
+}
+// console.log(simpleMultiplication(5));
+
+// ##################################################
+
+// Beginner - Reduce but Grow
+
+function grow(x) {
+  return x.reduce((a, c) => a * c, 1);
+}
+// console.log(grow([2, 2, 2, 2, 2, 2]));
+
+// ##################################################
+
+// Transportation on vacation
+
+function rentalCarCost(d) {
+  let total = d * 40;
+  if (d >= 7) {
+    return total - 50;
+  } else if (d >= 3) {
+    return total - 20;
+  } else {
+    return total;
+  }
+}
+
+console.log(rentalCarCost(10));
--- hw/mar22/javaScriptInfo_array_methods/js/main.js
+++ hw/mar22/javaScriptInfo_array_methods/js/main.js
@@ -30,5 +30,20 @@ console.log(camelize("background-color"));
 // ########################################################
 
 /*
+Filter range
+importance: 4
+Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
 
+The function should not modify the array. It should return the new array.
 */
+function filterRange(arr, a, b) {
+  let result = [];
+  arr.forEach((n) => {
+    if (n >= a && n <= b) {
+      result.push(n);
+    }
+  });
+  return result;
+}
+
+console.log(filterRange([5, 3, 8, 1], 3, 8));
