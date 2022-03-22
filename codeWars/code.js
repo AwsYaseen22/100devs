a96419233a4452b6f23f2f10980cc859c58c79cd
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-03-22 23:45:08 +0300

    more katas :|

--- codeWars/code.js
+++ codeWars/code.js
@@ -1026,6 +1026,40 @@ function multiTable(number) {
 function isPalindrome(x) {
   return x.toLowerCase().split("").reverse().join("") === x.toLowerCase();
 }
-console.log(isPalindrome("Abbas"));
+// console.log(isPalindrome("Abbas"));
+
+// ##################################################
+
+// What is between?
+
+function between(a, b) {
+  let result = [];
+  for (let i = a; i <= b; i++) {
+    result.push(i);
+  }
+  return result;
+}
+
+console.log(between(1, 4));
+
+// ##################################################
+
+// Cat years, Dog years
+
+var humanYearsCatYearsDogYears = function (humanYears) {
+  let catYears = 0;
+  let dogYears = 0;
+  if (humanYears === 1) {
+    catYears = 15;
+    dogYears = 15;
+  } else if (humanYears === 2) {
+    catYears = 15 + 9;
+    dogYears = 15 + 9;
+  } else {
+    catYears = 15 + 9 + (humanYears - 2) * 4;
+    dogYears = 15 + 9 + (humanYears - 2) * 5;
+  }
+  return [humanYears, catYears, dogYears];
+};
 
 // ##################################################
