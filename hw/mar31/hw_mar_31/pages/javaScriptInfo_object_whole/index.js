cb85c16ca944a03001277bd873399f64775853f8
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-03-28 23:04:07 +0300

    javascript.info Object the whole finish the constructor operator new

--- hw/mar31/hw_mar_31/pages/javaScriptInfo_object_whole/index.js
+++ hw/mar31/hw_mar_31/pages/javaScriptInfo_object_whole/index.js
@@ -14,6 +14,9 @@ Add the property surname with the value Smith.
 Change the value of the name to Pete.
 Remove the property name from the object.
 */
+console.log("************************");
+console.log("OBJECT TASKS");
+console.log("************************");
 let user = {};
 user.name = "John";
 user.surname = "Smith";
@@ -105,6 +108,11 @@ console.log(multiplyNumeric(menu));
 // Object references and copying
 // ######################################################
 // shallow copy of object
+
+console.log("************************");
+console.log("Object references and copying TASKS");
+console.log("************************");
+
 let user2 = {
   name: "John",
   sizes: {
@@ -163,6 +171,9 @@ When a function is called in the “method” syntax: object.method(), the value
 */
 
 // Tasks
+console.log("************************");
+console.log('Object methods, "this" TASKS');
+console.log("************************");
 /*
 (1)
 Using "this" in object literal
@@ -228,8 +239,8 @@ let calculator = {
 };
 
 calculator.read();
-alert(calculator.sum());
-alert(calculator.mul());
+console.log(calculator.sum());
+console.log(calculator.mul());
 
 /*
 (3)
@@ -250,7 +261,7 @@ let ladder = {
   },
   showStep: function () {
     // shows the current step
-    alert(this.step);
+    console.log(this.step);
     return this;
   },
 };
@@ -278,5 +289,97 @@ ladder.up().up().down().showStep().down().showStep(); // shows 1 then 0
 // ######################################################
 
 /*
+Summary
+Constructor functions or, briefly, constructors, are regular functions, but there’s a common agreement to name them with capital letter first.
+Constructor functions should only be called using new. Such a call implies a creation of empty this at the start and returning the populated one at the end.
+We can use constructor functions to make multiple similar objects.
 
+JavaScript provides constructor functions for many built-in language objects: like Date for dates, Set for sets and others that we plan to study.
 */
+
+// Tasks
+console.log("************************");
+console.log('Constructor, operator "new" TASKS');
+console.log("************************");
+/*
+(1)
+Two functions – one object
+importance: 2
+Is it possible to create functions A and B so that new A() == new B()?
+*/
+
+let obj = { name: "aws" };
+function A() {
+  return obj;
+}
+function B() {
+  return obj;
+}
+
+let a = new A();
+let b = new B();
+
+console.log("a === b: ", a === b); // true
+// If it is, then provide an example of their code.
+// by force the constructor to return object not this
+
+/*
+(2)
+Create new Calculator
+importance: 5
+Create a constructor function Calculator that creates objects with 3 methods:
+
+read() asks for two values using prompt and remembers them in object properties.
+sum() returns the sum of these properties.
+mul() returns the multiplication product of these properties.
+For instance:
+*/
+
+function Calculator() {
+  this.n1 = 0;
+  this.n2 = 0;
+  this.read = function () {
+    this.n1 = +prompt("Please enter n1", 0);
+    this.n2 = +prompt("Please enter n2", 0);
+  };
+  this.sum = function () {
+    return this.n1 + this.n2;
+  };
+  this.mul = function () {
+    return this.n1 * this.n2;
+  };
+}
+let calculator2 = new Calculator();
+calculator2.read();
+
+console.log("Sum=" + calculator2.sum());
+console.log("Mul=" + calculator2.mul());
+
+/*
+(3)
+Create new Accumulator
+importance: 5
+Create a constructor function Accumulator(startingValue).
+
+Object that it creates should:
+
+Store the “current value” in the property value. The starting value is set to the argument of the constructor startingValue.
+The read() method should use prompt to read a new number and add it to value.
+In other words, the value property is the sum of all user-entered values with the initial value startingValue.
+
+Here’s the demo of the code:
+*/
+
+function Accumulator(initialValue) {
+  this.value = initialValue;
+  this.read = function () {
+    this.value += +prompt("Please enter a number to add", 0);
+  };
+}
+
+let accumulator = new Accumulator(1); // initial value 1
+
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+
+console.log(accumulator.value); // shows the sum of these values
