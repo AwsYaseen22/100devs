7986472c995fdb65ddc9fc98875e68fe5580b2c7
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-04-11 01:23:08 +0300

    experiment with the cocktails with carousel broken not work yet

--- Day26/class26-materials/cocktails_carousel/js/main.js
+++ Day26/class26-materials/cocktails_carousel/js/main.js
@@ -1,63 +1,120 @@
 // The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
-/*
-
-
-				
-						<h2 class="carousel-item__subtitle">The grand moment </h2>
-						<h1 class="carousel-item__title">Le tour</h1>
-						<p class="carousel-item__description">Sed ut perspiciatis unde omnis iste natus error sit
-							voluptatem
-							accusantium doloremque laudantium, totam rem aperiam.</p>
-						<a href="#" class="carousel-item__btn">Explore the tour</a>
-					</div>
-				</div>
-			</div>
- */
+// create array to contains all concktaisl data
+// let cockArr = [];
+// function CockDetails(name, image, ingredients, instructions) {
+//   this.name = name;
+//   this.image = image;
+//   this.ingredients = ingredients;
+//   this.instructions = instructions;
+// }
 
 let carousel = document.querySelector(".carousel");
 let current = 0;
+// const items = [];
 
-function createList(cocktailsList) {
-  for (let i = 0; i < cocktailsList.length; i++) {
-    let div = document.createElement("div");
-    div.classList.add("carousel-item");
-    let image = `<img src=${cocktailsList[i].strDrinkThumb}>`;
-    let imageDiv = `
-          <div class="carousel-item__image">
-            ${image}
-          </div>`;
-    div.innerHTML = `
-      <div class="carousel-item__image">
-        ${image}
-      </div>
+function createList(name, image, ingredients, instructions) {
+  // console.log(cocktailsList);
+  // for (let i = 0; i < cocktailsList.length; i++) {
+  let div = document.createElement("div");
+  div.classList.add("carousel-item");
+  let imageDiv = document.createElement("div");
+  imageDiv.classList.add("carousel-item__image");
+  imageDiv.style.backgroundImage = `url(${image})`;
+  div.innerHTML = `
+      ${imageDiv.outerHTML}
       <div class="carousel-item__info">
         <div class="carousel-item__container">
-          <h1 class="carousel-item__title">${cocktailsList[i].strDrink}</h1>
-
+          <h1 class="carousel-item__title">${name}</h1>
         </div>
       </div>
-        <span class='hidden'>${cocktailsList[i].idDrink}</span>
+        <span class='hidden'>${instructions}</span>
     `;
-    carousel.appendChild(div);
-  }
+  carousel.appendChild(div);
+  return div;
+  // console.log(div);
+  // console.log(carousel);
+  // }
+  // return carousel;
 }
 
-let cocktails;
+// let cocktails;
 fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic")
   .then((response) => response.json())
   .then((data) => {
     // console.log(data.drinks);
-    cocktails = data.drinks;
-    createList(cocktails);
-    let items = document.querySelectorAll(".carousel-item");
+    let cocktails = data.drinks;
+    // console.log(cocktails);
+    // createList(cocktails);
+    // let items = document.querySelectorAll(".carousel-item");
+    // console.log(items);
+    return cocktails;
+  })
+  .then((nonAlcohol) => {
+    const items = [];
+    nonAlcohol.forEach((drink) => {
+      fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drink.idDrink}`
+      )
+        .then((response) => response.json())
+        .then((data) => data.drinks[0])
+        .then((drink) => {
+          console.log(drink);
+          let ing = getIngredients(drink);
+          items.push(
+            createList(
+              drink.strDrink,
+              drink.strDrinkThumb,
+              ing,
+              drink.strInstructions
+            )
+          );
+          // );
+        });
+    });
+    return items;
+    // for (const drink of nonAlcohol) {
+    // fetch(
+    //   `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drink.idDrink}`
+    // )
+    //   .then((response) => response.json())
+    //   .then((data) => data.drinks[0])
+    //   .then((drink) => {
+    //     let ing = getIngredients(drink);
+    //     items.push(
+    //       createList(
+    //         drink.strDrink,
+    //         drink.strDrinkThumb,
+    //         ing,
+    //         drink.strInstructions
+    //       )
+    //     );
+
+    // cockArr.push(
+    //   new CockDetails(
+    //     drink.strDrink,
+    //     drink.strDrinkThumb,
+    //     ing,
+    //     drink.strInstructions
+    //   )
+    // );
+    // return cockArr;
+    // });
+    // .then((arr) => {
+    //   console.log(arr);
+    // });
+    // }
+    // let items = document.querySelectorAll(".carousel-item");
+    // return items;
+  })
+  .then((items) => {
     console.log(items);
     return items;
   })
-  .then((x) => {
-    console.log(x[0]);
-    x[0].classList.add("active");
-    let total = x.length;
+  .then((items) => {
+    console.log(items[0]);
+    items[0].classList.add("active");
+    let total = items.length;
     console.log(total);
     // let current = 0;
     document.querySelector("#moveRight").addEventListener("click", () => {
@@ -80,14 +137,15 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic")
         slide = total - 1;
         current = total - 1;
       }
-      x[prev].classList.remove("active");
-      x[slide].classList.add("active");
+      items[prev].classList.remove("active");
+      items[slide].classList.add("active");
       setTimeout(function () {}, 800);
       console.log("current: ", current);
       console.log("prev: ", prev);
     }
-    x.forEach((l) => l.addEventListener("click", () => clickItem(l)));
   });
+
+// x.forEach((l) => l.addEventListener("click", () => clickItem(l)));
 
 // function clickItem(e) {
 //   console.log("clicked");
@@ -114,18 +172,18 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic")
 //   document.querySelector(".details").classList.toggle("hidden");
 // }
 
-// function getIngredients(ing) {
-//   console.log(ing);
-//   //   let leng = Object.keys(ing).length;
-//   let ingredients = "";
-//   for (let i = 1; i <= 15; i++) {
-//     // console.log(ing[`strIngredient${i}`]);
-//     // console.log(ing[`strMeasure${i}`]);
-//     ing[`strIngredient${i}`]
-//       ? (ingredients += `${ing[`strIngredient${i}`]} (${
-//           ing[`strMeasure${i}`]
-//         })\n`)
-//       : "";
-//   }
-//   return ingredients;
-// }
+function getIngredients(ing) {
+  // console.log(ing);
+  //   let leng = Object.keys(ing).length;
+  let ingredients = "";
+  for (let i = 1; i <= 15; i++) {
+    // console.log(ing[`strIngredient${i}`]);
+    // console.log(ing[`strMeasure${i}`]);
+    ing[`strIngredient${i}`]
+      ? (ingredients += `${ing[`strIngredient${i}`]} (${
+          ing[`strMeasure${i}`]
+        })\n`)
+      : "";
+  }
+  return ingredients;
+}
