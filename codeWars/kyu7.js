614a668ab002bcd5c9c6e299105b20ac4f650074
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-04-10 16:35:33 +0300

    solve first 7kyu kata

---
+++ codeWars/kyu7.js
@@ -0,0 +1,8 @@
+// Vowel Count
+
+function getCount(str) {
+  return str.match(/[aeiou]/gi) ? str.match(/[aeiou]/gi).length : 0;
+}
+// console.log(getCount("my pyx"));
+
+// ##################################################
