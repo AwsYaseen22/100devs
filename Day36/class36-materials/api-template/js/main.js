c79ba3b9259ab58df507b1213f996a7a693c4ad5
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-05-27 17:22:51 +0300

    start class 37

--- Day36/class36-materials/api-template/js/main.js
+++ Day36/class36-materials/api-template/js/main.js
@@ -1,1 +1,15 @@
 //Make an api request using async await
+
+let getNewPhoto = async () => {
+  try {
+    let res = await fetch("https://dog.ceo/api/breeds/image/random");
+    let data = await res.json();
+    console.log(data);
+    img.src = data.message;
+  } catch (error) {
+    console.log("the error: ", error);
+  }
+};
+
+let img = document.querySelector("img");
+document.querySelector("button").addEventListener("click", getNewPhoto);
